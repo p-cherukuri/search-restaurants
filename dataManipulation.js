e342d4b266e880b60dd785d8c8005dea42298851
p-cherukuri <phani.cheruk@gmail.com>
2017-11-08 13:39:36 -0500

    added dataset directory, node test binary script 'testscript', csvtojson package

---
+++ dataManipulation.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const csv = require("csvtojson");
+const restaurantsInfoPath = "./dataset/restaurants_info.csv";
+const restaurantsList = require("./dataset/restaurants_list.json");
